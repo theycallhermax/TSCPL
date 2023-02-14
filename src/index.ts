4bd89c40039cfcaf497050e425a9d1e2665bc42d
M.D. Walters <popularvsofficial@gmail.com>
2023-02-14 02:41:12 +0000

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,17 @@ import * as yargs from "yargs";
 import * as fs from "fs";
 import chalk from "chalk";
 import { exit } from "process";
+import { exec } from "child_process";
 
-console.log(chalk.hex("#0077ff").bold("TSCPL v1.0.0"));
+function appendFileCallback(err): void {
+    if (err) {
+      console.error(chalk.redBright(`ERROR: ${err}`));
+      console.error(chalk.redBright(`Unsuccessfully compiled ${chalk.redBright.bold(yargs.argv._[0])}.`));
+      exit(1);
+    }
+}
 
+console.log(chalk.hex("#0077ff").bold("TSCPL v1.2.0"));
 
 if (yargs.argv._[0] === undefined) {
     console.error(chalk.redBright("ERROR: Input file is required"));
@@ -16,15 +24,45 @@ if (yargs.argv._[0] === undefined) {
 let file: string[] = fs.readFileSync(yargs.argv._[0], "utf-8").split("\n");
 
 console.log(chalk.yellowBright(`Compiling ${chalk.yellowBright.bold(yargs.argv["_"][0])}...`));
+fs.writeFileSync(`${yargs.argv._[0]}.ts`, "");
 
 for (let i in file) {
     if (file[i].split(" ")[0] === "outln") {
-        fs.writeFileSync(`${yargs.argv._[0].split(".acpl")}.js`, `console.log(${file[i].split(" ").slice(1, file[i].split(" ").length).join(" ")});\n`);
+        fs.appendFile(`${yargs.argv._[0]}.ts`, `console.log(${file[i].split(" ").slice(1, file[i].split(" ").length).join(" ")});\n`, appendFileCallback);
         continue;
-    } else if (file[i].split(" ")[0].startsWith("#")) {
-        fs.writeFileSync(`${yargs.argv._[0].split(".acpl")}.js`, `//${file[i].split(" ").slice(1, file[i].split(" ").length).join(" ")}\n`);
+    } else if (file[i].split(" ")[0] === "str") {
+        fs.appendFile(`${yargs.argv._[0]}.ts`, `let ${file[i].split(" ")[1]}: string = ${file[i].split(" ").slice(2, file[i].split(" ").length).join(" ")};\n`, appendFileCallback);
         continue;
+    } else if (file[i].split(" ")[0] === "int") {
+        fs.appendFile(`${yargs.argv._[0]}.ts`, `let ${file[i].split(" ")[1]}: number = ${file[i].split(" ").slice(2, file[i].split(" ").length).join(" ")};\n`, appendFileCallback);
+        continue;
+    } else if (file[i].split("")[0] === "#") {
+        fs.appendFile(`${yargs.argv._[0]}.ts`, `//${file[i].split("").slice(1, file[i].split("").length).join("")}\n`, appendFileCallback);
+        continue;
+    } else if (file[i].split(" ")[0] === "outs") {
+        fs.appendFile(`${yargs.argv._[0]}.ts`, `console.log(${file[i].split(" ")[1]});\n`, appendFileCallback);
+        continue;
+    } else if (file[i] === "") {
+        fs.appendFile(`${yargs.argv._[0]}.ts`, "\n", appendFileCallback);
+        continue;
+    } else {
+        console.log(chalk.redBright(`ERROR: Invalid syntax at line ${parseInt(i) + 1} of ${chalk.redBright.bold(yargs.argv._[0])}`));
+        console.error(chalk.redBright(`Unsuccessfully compiled ${chalk.redBright.bold(yargs.argv._[0])}.`));
+        exit(1);
     }
 }
 
-console.log(chalk.greenBright(`Successfully compiled ${chalk.greenBright.bold(yargs.argv._[0])}.`));
+console.log(chalk.greenBright(`Successfully compiled ${chalk.greenBright.bold(yargs.argv._[0])} as ${chalk.greenBright.bold(`${yargs.argv._[0]}.ts`)}.`));
+console.log(chalk.yellowBright(`Running ${chalk.yellowBright.bold(`${yargs.argv._[0]}.ts`)}...`));
+
+if (yargs.argv.run === true) {
+    exec(`npx ts-node ${yargs.argv._[0]}.ts`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(error);
+            exit(1);
+        }
+
+        console.log(stdout);
+        console.error(stderr);
+    });
+}
