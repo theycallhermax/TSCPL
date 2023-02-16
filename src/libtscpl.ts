860d7626ea5b8a4f9daccf712b3c220eec2f01ad
M.D. Walters <popularvsofficial@gmail.com>
2023-02-16 12:44:08 -0500

    Haha lol

--- src/libtscpl.ts
+++ src/libtscpl.ts
@@ -58,7 +58,7 @@ export function compile(file: string, output_file_name: string): void {
                 variables.push(file_split[i].split(" ").slice(2, file_split[i].split(" ").length)[j]);
             }
 
-            fs.appendFile(output_file_name, `function ${file_split[i].split(" ")[1]}(${file_split[i].split(" ").slice(2, file_split[i].split(" ").length).join(", ")})`, callback);
+            fs.appendFile(output_file_name, `function ${file_split[i].split(" ")[1]}(${file_split[i].split(" ").slice(2, file_split[i].split(" ").length).join(": any, ")}: any)`, callback);
             continue;
         } else if (file_split[i].split(" ")[0] === ":") {
             fs.appendFile(output_file_name, ` {\n`, callback);
