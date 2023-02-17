2dc11f28538d7bab99e690c34def5fcc47c9a6e3
M.D. Walters <popularvsofficial@gmail.com>
2023-02-17 00:09:12 -0500

    Change it to module

--- src/libtscpl.ts
+++ src/libtscpl.ts
@@ -18,7 +18,7 @@ export function compile(file: string, output_file_name: string): void {
 
     fs.writeFileSync(output_file_name, "");
 
-    if (file_split[0].split(" ")[0] === "package") {
+    if (file_split[0] === "module") {
         is_package = true;
     }
 
@@ -44,7 +44,7 @@ export function compile(file: string, output_file_name: string): void {
                 fs.appendFile(output_file_name, `let ${file_split[i].split(" ")[1]}: number = ${file_split[i].split(" ").slice(2, file_split[i].split(" ").length).join(" ")};\n`, callback);
             }
             continue;
-        } else if (file_split[i].split("")[0] === "#" || file_split[i] === "" || file_split[i] === "package") {
+        } else if (file_split[i].split("")[0] === "#" || file_split[i] === "" || file_split[i] === "module") {
             continue;
         } else if (file_split[i].split(" ")[0] === "outs" || file_split[i].split(" ")[0] === "outi") {
             if (!(variables.includes(file_split[i].split(" ")[1]))) {
