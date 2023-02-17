2bc9bba51304c10a558ad0901874aa8b3d04e251
M.D. Walters <popularvsofficial@gmail.com>
2023-02-17 01:37:18 -0500

    ye

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ if (yargs.argv._[0] === undefined) {
     console.log("");
     console.log(chalk.whiteBright(`To learn more, see ${chalk.blue.bold("https://github.com/mdwalters/TSCPL#readme")}`));
     console.log(chalk.whiteBright(`To learn more about libtscpl, see ${chalk.blue.bold("https://github.com/mdwalters/TSCPL#libtscpl")}`));
-    console.log(chalk.whiteBright(`To learn ACPL, see ${chalk.blue.bold("https://hackmd.io/Fzlzx3DCRSe2CB4-ABRFcQ")}`));
+    console.log(chalk.whiteBright(`To learn ACPL, see ${chalk.blue.bold("https://hackmd.io/@mdwalters/acpl")}`));
     exit(0);
 }
 
