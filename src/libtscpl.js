62bd4034236491357e9cf6252f16e223864c299a
Replit user <>
2023-02-26 19:47:07 +0000

    Add more stuff to `.npmignore` Why Replit, why?

--- src/libtscpl.js
+++ src/libtscpl.js
@@ -1,30 +1,4 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.compile = void 0;
-const fs = __importStar(require("fs"));
+import * as fs from "fs";
 function callback() {
     return;
 }
@@ -34,7 +8,7 @@ function callback() {
 * @param file The ACPL file to compile
 * @param output_file_name The name of the output file
 */
-function compile(file, output_file_name) {
+export function compile(file, output_file_name) {
     let file_split = fs.readFileSync(file, "utf-8").split("\n");
     let variables = [];
     let functions = [];
@@ -178,4 +152,3 @@ function compile(file, output_file_name) {
         }
     }
 }
-exports.compile = compile;
