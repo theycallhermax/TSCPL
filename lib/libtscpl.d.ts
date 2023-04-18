cb9f5c390a2b8e45d7637ae861ad4dc5d0951f5e
M.D. Walters <popularvsofficial@gmail.com>
2023-04-17 23:52:22 -0400

    Update libtscpl.d.ts

--- lib/libtscpl.d.ts
+++ lib/libtscpl.d.ts
@@ -1,3 +1,5 @@
-declare function compile(file: string, output_file_name: string): void;
+import { options } from "./interfaces";
+
+declare function compile(file: string, output_file_name: string, options: options): void;
 
 export = compile;
